c25963c3018acfa9384e12245b76139ea6ebcc6f
woogyeom <woogyeom99@gmail.com>
2024-03-22 22:56:06 -0400

    created

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module rucafe.ordermanager.cs213project4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens rucafe.ordermanager.cs213project4 to javafx.fxml;
+    exports rucafe.ordermanager.cs213project4;
+}
