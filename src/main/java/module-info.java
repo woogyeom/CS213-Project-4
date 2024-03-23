e25b9ec28d794769156b4a0ff88bc3a4b38a6c91
woogyeom <woogyeom99@gmail.com>
2024-03-22 23:57:00 -0400

    1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module rucafe.ordermanager.cs213project4 {
     requires javafx.fxml;
 
 
-    opens rucafe.ordermanager.cs213project4 to javafx.fxml;
-    exports rucafe.ordermanager.cs213project4;
+    opens rucafe.ordermanager to javafx.fxml;
+    exports rucafe.ordermanager;
 }
--- src/main/java/rucafe/ordermanager/HelloApplication.java
+++ src/main/java/rucafe/ordermanager/HelloApplication.java
@@ -1,4 +1,4 @@
-package rucafe.ordermanager.cs213project4;
+package rucafe.ordermanager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
