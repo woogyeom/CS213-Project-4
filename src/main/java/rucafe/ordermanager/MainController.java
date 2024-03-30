01dd3d0ff609d73a71a3a0bac1e1915abb6774d6
woogyeom <woogyeom99@gmail.com>
2024-03-30 00:01:15 -0400

    Main View

---
+++ src/main/java/rucafe/ordermanager/MainController.java
@@ -0,0 +1,34 @@
+package rucafe.ordermanager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onOrderDonutsButtonClick() {
+
+    }
+
+    @FXML
+    protected void onOrderCoffeeButtonClick() {
+
+    }
+
+    @FXML
+    protected void onOrderSandwichesButtonClick() {
+
+    }
+
+    @FXML
+    protected void onOrderCurrentOrderButtonClick() {
+
+    }
+
+    @FXML
+    protected void onOrderAllOrdersButtonClick() {
+
+    }
+}
