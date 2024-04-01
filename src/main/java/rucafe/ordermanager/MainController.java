473757c009282eb602108f31a2bd5fdfb5ea9df4
woogyeom <woogyeom99@gmail.com>
2024-04-01 18:05:32 -0400

    Added an image to a button

--- src/main/java/rucafe/ordermanager/MainController.java
+++ src/main/java/rucafe/ordermanager/MainController.java
@@ -4,15 +4,22 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class MainController {
+    public Button allOrdersButton;
 
-    // Gotta change the buttons to image buttons
+    @FXML
+    protected void initialize() {
+        ImageView imageView = new ImageView(Objects.requireNonNull(getClass().getResource("all-orders.png")).toExternalForm());
+        allOrdersButton.setGraphic(imageView);
+    }
 
     @FXML
     protected void onOrderDonutsButtonClick() {
