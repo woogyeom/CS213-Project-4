e41965835322b995684574235a2145c410418906
Aravind <aravindchundu@gmail.com>
2024-04-01 21:15:15 -0400

    Did Junit. Also fixed prices in sandwichprotein (fish and chicken were reversed)

--- src/main/java/rucafe/ordermanager/SandwichProtein.java
+++ src/main/java/rucafe/ordermanager/SandwichProtein.java
@@ -2,8 +2,8 @@ package rucafe.ordermanager;
 
 public enum SandwichProtein {
     BEEF(10.99),
-    FISH(8.99),
-    CHICKEN(9.99);
+    FISH(9.99),
+    CHICKEN(8.99);
 
     private final double price;
 
